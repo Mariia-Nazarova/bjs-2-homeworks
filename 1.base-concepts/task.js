baebe6906f89dfb4343c9f903093731bca66a1ef
Mariia Nazarova <Djungary@mail.ru>
2024-03-14 14:15:29 +0300

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,30 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
+  let d = Math.pow(b, 2) - 4 * a * c;
   
-  return arr;
+  let r1;
+  let r2;
+
+  if (d > 0) {
+    r1 = (-b + Math.sqrt(d)) / (2 * a);    r2 = (-b - Math.sqrt(d)) / (2 * a);
+    let arr = [r1,r2];
+    console.log(arr);
+  }
+
+  else 
+  if (d == 0) {
+    r1 = r2 = -b / (2 * a);
+    let arr = [r1];
+    console.log(arr);
+  }
+
+  else {
+      console.log([]);
+  }
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
 }
+
